57825b51cfbd92c23bee64f2f8f5ae033c12bc52
Aman Kumar <103573041+amankumar-in@users.noreply.github.com>
2025-02-12 18:08:38 +0530

    loading

--- MediumClone/script.js
+++ MediumClone/script.js
@@ -27,3 +27,13 @@ function toggleMobileMenu() {
         mobileNav.style.width = "250px"; // Adjust width as needed
     }
 }
+
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
