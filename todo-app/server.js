b617db22cd93772799b5f24f4e78492628ae404e
Aman Kumar <103573041+amankumar-in@users.noreply.github.com>
2025-04-10 18:10:15 +0530

    todo init

---
+++ todo-app/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Test route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend is connected!" });
+});
+
+// Root route to serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
